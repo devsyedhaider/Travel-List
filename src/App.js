17b72a1ad17e1a8a0f202e6acd646eac8967e4b4
devsyedhaider <devalihaider86@gmail.com>
2026-02-18 14:38:15 +0500

    List Rendring into Ui and Some Style Changes into Ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+  { id: 4, description: "Clothes", quantity: 3, packed: false },
+];
+
 export default function App() {
   return (
     <div className="apps">
@@ -22,7 +29,30 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item: { description, quantity, packed } }) {
+  return (
+    <>
+      <li>
+        <span style={packed ? { textDecoration: "line-through" } : {}}>
+          <span style={{ marginRight: ".7rem" }}>{quantity}</span>
+          {description}
+        </span>
+        <button>❌</button>
+        {packed}
+      </li>
+    </>
+  );
 }
 
 function Stats() {
