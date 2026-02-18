852b42a1e66765dcf8821b938228035c182e8dba
devsyedhaider <devalihaider86@gmail.com>
2026-02-18 14:00:30 +0500

    Add Componnets into UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-function App() {
+export default function App() {
   return (
-    <>
-      <h1>hassan</h1>
-      <h1>haider</h1>
-    </>
+    <div className="apps">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😄 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
