fc0c33cc8601558a4275a7cffded91be563f5955
devsyedhaider <devalihaider86@gmail.com>
2026-02-23 17:16:56 +0500

    DeleteItem

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDelete(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="apps">
       <Logo />
       <Form onAddItem={handleAdd} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDelete} />
       <Stats />
     </div>
   );
@@ -61,28 +65,27 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item: { description, quantity, packed } }) {
+function Item({ item, onDeleteItem }) {
   return (
     <>
       <li>
-        <span style={packed ? { textDecoration: "line-through" } : {}}>
-          <span style={{ marginRight: ".7rem" }}>{quantity}</span>
-          {description}
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          <span style={{ marginRight: ".7rem" }}>{item.quantity}</span>
+          {item.description}
         </span>
-        <button>❌</button>
-        {packed}
+        <button onClick={() => onDeleteItem(item.id)}>❌</button>
       </li>
     </>
   );
